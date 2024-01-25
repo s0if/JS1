ca7b65ff5201160a753270c306cd737e9bdbde20
s0if <saifalkomi@gmail.com>
2024-01-25 16:32:12 +0200

    end edit

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 var rigester = document.querySelector(".registerForm")
-console.log(rigester)
+var exchange = document.querySelector(".exchange")
+var input = document.querySelector(".input")
 var coins = ""
 rigester.onsubmit = function (e) {
     e.preventDefault();
     var elements = e.target.elements;
-    var dollar = elements[1];
-    console.log(dollar[0])
-    console.log(dollar[0] == true)
-    if (dollar[0] == true)
-        console.log(elements[0].text * 3.14)
+    var selcet = exchange.value;
+    if (selcet == "dollar")
+        coins = input.value * "3.7";
+    else if (selcet == "dinar")
+        coins = input.value * "5.2";
     else
-    console.log("wring")
-
-    //     e.preventDefault();
-    //     var elements = e.target.elements;
-    //     var val=elements[exchange].target.elements[dollar].value
-    //     if (val == true)
-    //         var coin =val.text * 3.14;
-    //     document.querySelector(".result").textContent = (coin)
+        coins = input.value;
+    let p=document.querySelector(".result");
+    p.textContent = (
+        `
+    ${input.value} = ${coins} ${selcet}
+    `
+    );
+    input.value = "";
 }
